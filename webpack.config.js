703e571e8cd127c39764607dfef05d52c80e6b2d
NameJP <815467795@qq.com>
2018-08-19 11:11:57 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output:{
+        path: path.resolve(__dirname, './dist'),
+        filename: 'my-first-webpack.bundle.js'
+      },
+      plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: './index.html'
+            }
+        )
+      ],
+  };
